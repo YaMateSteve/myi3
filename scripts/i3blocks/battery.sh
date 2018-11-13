08a0fe2ec65ab81ca358caa726401a048a3de5ce
YaMateSteve <stevendean8@outlook.com>
2018-11-13 23:01:19 +1000

    i3 config

---
+++ scripts/i3blocks/battery.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get reamining battery percentage
+bat_percent=$(acpi | grep -o '[0-9]\{2,3\}[%]')
+bat=$(acpi | grep -o '[0-9]\{2,3\}[%]' | grep -o '[0-9]\{2,3\}')
+
+if [ $bat -gt 90 ]; then
+	echo "_$bat_percent"
+elif [ $bat -gt 75 ] && [ $bat -le 90 ]; then
+	echo "_$bat_percent"
+elif [ $bat -gt 50 ] && [ $bat -le 75 ]; then
+	echo "_$bat_percent"
+elif [ $bat -gt 20 ] && [ $bat -le 50 ]; then
+	echo "_$bat_percent"
+else
+	echo "_$bat_percent"
+fi
