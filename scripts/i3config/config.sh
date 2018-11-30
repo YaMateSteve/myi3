b48e8fdac4614340fee85f7f4641a057d3c6ac9e
YaMateSteve <stevendean8@outlook.com>
2018-11-30 15:07:46 +1000

    i3 config

--- scripts/i3config/config.sh
+++ scripts/i3config/config.sh
@@ -1,4 +1,4 @@
 count=`ps aux | grep -c gedit`
 if [ $count -eq 1 ]; then
-     gedit ~/.config/i3/config + ~/.Xresources + ~/.bashrc + ~/myi3/keybindings.txt 
+     gedit ~/.config/i3/config + ~/.Xresources + ~/.bashrc + ~/myi3/keybindings.txt + ~/.config/i3/i3blocks.conf 
 fi
